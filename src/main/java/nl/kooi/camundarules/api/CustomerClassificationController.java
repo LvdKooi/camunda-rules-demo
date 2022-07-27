600944c97f17e3efd07114e6493e9f1bb81be363
Laurens van der Kooi <laurens.van.der.kooi@rabobank.nl>
2022-07-27 15:14:12 +0200

    Endpoint for starting Camunda Process

--- src/main/java/nl/kooi/camundarules/api/CustomerClassificationController.java
+++ src/main/java/nl/kooi/camundarules/api/CustomerClassificationController.java
@@ -1,13 +1,14 @@
 package nl.kooi.camundarules.api;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import nl.kooi.camundarules.api.dto.ClassificationOutcome;
 import nl.kooi.camundarules.api.dto.CustomerLabelDto;
 import nl.kooi.camundarules.domain.CustomerLabelService;
 import nl.kooi.camundarules.domain.CustomerTopLabelService;
+import org.camunda.bpm.engine.RuntimeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Comparator;
@@ -16,14 +17,15 @@ import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/classification-outcome")
+@Slf4j
 public class CustomerClassificationController {
 
     private final CustomerLabelService customerLabelService;
     private final CustomerTopLabelService customerTopLabelService;
+    private final RuntimeService camundaRuntime;
 
 
-    @GetMapping
+    @GetMapping("/classification-outcome")
     public List<ClassificationOutcome> getClassificationOutcomes() {
         var labelsPerCustomerId = customerLabelService.getCustomerLabels().stream().map(Mapper::map).sorted(Comparator.comparingLong(CustomerLabelDto::getCustomerId)).collect(Collectors.groupingBy(CustomerLabelDto::getCustomerId));
 
@@ -34,9 +36,17 @@ public class CustomerClassificationController {
                 .collect(Collectors.toList());
     }
 
-    @PostMapping("/clear-all")
+    @PostMapping("/classification-outcome/clear-all")
     public void clearAllClassifications() {
         customerLabelService.clearAll();
         customerTopLabelService.clearAll();
+    }
+
+    @PostMapping("/start-classification")
+    public void startClassificationProcess() {
+        log.info("Clearing previous outcome.");
+        customerTopLabelService.clearAll();
+        var process = camundaRuntime.startProcessInstanceByKey("classification_process");
+        log.info("Process started with id: {}", process.getId());
     }
 }
